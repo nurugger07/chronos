021e15fa7b3e1474077856c9b6476949cb3c6bf7
Johnny Winn <dev+nurugger07@hashrocket.com>
2013-08-20 15:13:51 -0400

    Check for valid date

--- lib/chronos.ex
+++ lib/chronos.ex
@@ -1,22 +1,42 @@
 defmodule Chronos do
 
+  alias :calendar, as: Cal
+  alias :erlang, as: Erl
+
+  @default_format "~4..0B-~1..0B-~2..0B"
+
   def today do
-    :erlang.date
+    Erl.date
+  end
+
+  def now do
+    Erl.now |> Cal.now_to_datetime
   end
 
   def year(date) do
-    { year, _, _ } = date
+    { year, _, _ } = validate(date)
     year
   end
 
   def month(date) do
-    { _, month, _ } = date
+    { _, month, _ } = validate(date)
     month
   end
 
   def day(date) do
-    { _, _, day } = date
+    { _, _, day } = validate(date)
     day
   end
 
+  def valid_date?(date), do: Cal.valid_date(date)
+
+  defp validate(date) do
+    cond do
+      valid_date?(date) -> date
+      :else ->
+        raise ArgumentError, message: "Invalid date argument #{inspect date}"
+    end
+  end
+
+  defp date_to_list(date) when is_tuple(date), do: tuple_to_list(date)
 end
--- test/chronos_test.exs
+++ test/chronos_test.exs
@@ -9,19 +9,42 @@ defmodule ChronosTest do
     assert today == @today
   end
 
+  test :now do
+    assert now == :calendar.now_to_datetime(:erlang.now)
+  end
+
+  test :valid_date? do
+    assert valid_date?({ 2012, 12, 21 })
+    assert valid_date?({ 2008, 2, 29 })
+    refute valid_date?({ 2009, 2, 29 })
+    refute valid_date?({ 0000, -2, 29 })
+  end
+
   test "today's year" do
     { year, _, _ } = @today
     assert today |> year  == year
+
+    assert_raise ArgumentError, "Invalid date argument {2013, 13, 41}", fn ->
+      year({2013, 13, 41})
+    end
   end
 
   test "today's month" do
     { _, month, _ } = @today
     assert today |> month == month
+
+    assert_raise ArgumentError, "Invalid date argument {2013, 13, 41}", fn ->
+      month({2013, 13, 41})
+    end
   end
 
   test "today's day" do
     { _, _, day } = @today
     assert today |> day == day
+
+    assert_raise ArgumentError, "Invalid date argument {2013, 13, 41}", fn ->
+      day({2013, 13, 41})
+    end
   end
 
 end
