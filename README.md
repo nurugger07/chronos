1cd9fa1991837336b43091d30d29f02daea77205
Johnny Winn <dev+nurugger07@hashrocket.com>
2013-08-21 19:52:00 -0400

    Update documentation

--- README.md
+++ README.md
@@ -1,3 +1,79 @@
 # Chronos
 
-An Elixir library for handling dates
+An Elixir library for handling dates. Chronos can be used in both production and testing to quickly
+determine a date.
+
+## Using
+
+You can add Chronos as a dependency in your `mix.exs` file. Since it only requires Elixir and Erlang there are no other dependencies.
+
+```
+def deps do
+  [ { :chronos, github: "nurugger07/chronos" } ]
+end
+```
+
+Then run `mix deps.get` in the shell to fetch and compile the dependencies
+
+To use the Chronos date features in your project you can import the Chronos module or call the functions directly.
+
+```
+defmodule YourModule do
+  
+  import Chronos
+
+  def get_today do
+    today
+  end
+end
+```
+
+or you can call functions without the import
+
+```
+defmodule YourModule do
+  def get_today do
+    Chronos.today
+  end
+end
+```
+
+There are a number of functions to help with dates including below are some of the current APIs:
+
+```
+# yesterday without a date assumes you want the day before the current date
+# current date is {2012, 12, 21}
+iex(1)> Chronos.yesterday
+{2012, 12, 20}
+
+iex(2)> Chronos.tomorrow
+{2012, 12, 22}
+```
+
+You can find the date for days or weeks in the past or future:
+
+```
+iex(1)> Chronos.days_ago(3)
+{2012, 12, 18}
+
+iex(2)> Chronos.weeks_ago(5)
+{2012, 11, 16}
+```
+
+## Use in Testing
+
+Chronos is helpful in testing date based assertions because you can assign a default date or pass in a date to base the calculations on.
+
+```
+defmodule TestingModule do
+  use Chronos, date: {2012, 12, 21}
+end
+
+```
+If the date option is set the default date for all functions will be that date. 
+
+## Coming Soon
+
+* More date features like begining_of_week, end_of_week, 
+* More Time features
+* Time Date formatting options
--- lib/chronos.ex
+++ lib/chronos.ex
@@ -2,20 +2,100 @@ defmodule Chronos do
 
   import Chronos.Validation
 
+  @doc """
+    Chronos is an Elixir library for working with dates and times. 
+
+    iex(1)> Chronos.today
+    {2013, 8, 21}
+
+  """
   def today, do: :erlang.date
 
   def now, do: :erlang.now |> :calendar.now_to_datetime
 
+  @doc """
+    The year function allows you to extract the year from a date tuple
+
+    iex(1)> Chronos.year({2013, 8, 21})
+    2013
+
+    iex(2)> {2012, 12, 21} |> Chronos.year
+    2012
+  """
   def year(date), do: validate(date) |> _extract_seg(:year)
 
+  @doc """
+    The month function allows you to extract the month from a date tuple
+
+    iex(1)> Chronos.month({2013, 8, 21})
+    8
+
+    iex(2)> {2012, 12, 21} |> Chronos.month
+    8
+  """
   def month(date), do: validate(date) |> _extract_seg(:month)
 
+  @doc """
+    The day function allows you to extract the day from a date tuple
+
+    iex(1)> Chronos.day({2013, 8, 21})
+    21
+
+    iex(2)> {2012, 12, 21} |> Chronos.day
+    21
+  """
   def day(date), do: validate(date) |> _extract_seg(:day)
 
+  @doc """
+    The yesterday function is based on the current date
+
+    iex(1)> Chronos.yesterday
+    {2013, 8, 20}
+
+    or you can pass it a date:
+
+    iex(2)> {2012, 12, 21} |> Chronos.yesterday
+    {2012, 12, 20} 
+
+  """
   def yesterday(date // :erlang.date), do: calculate_date_for_days(date, -1)
 
+  @doc """
+    The tomorrow function is based on the current date
+
+    iex(1)> Chronos.tomorrow
+    {2013, 8, 22}
+
+    or you can pass it a date:
+
+    iex(2)> {2012, 12, 21} |> Chronos.tomorrow
+    {2012, 12, 22} 
+
+  """
   def tomorrow(date // :erlang.date), do: calculate_date_for_days(date, 1)
 
+  @doc """
+    The following functions all have similar behavior. The days_ago/2 and weeks_ago/2
+    functions take a integer representing the number of days or weeks in the past and
+    return the corresponding date. There is a optional argument for a date to base the
+    calculation on but if no date is provided then the current date is used.
+
+    iex(1)> Chronos.days_ago(5)
+    {2013, 8, 16}
+
+    iex(2)> Chronos.weeks_ago(3)
+    {2013, 3, 31}
+
+    The days_from/2 and weeks_from/2 return a future date calculated by the number
+    of days or weeks. There is a optional argument for a date to base the
+    calculation on but if no date is provided then the current date is used.
+
+    iex(1)> Chronos.days_from(5)
+    {2013, 8, 26}
+
+    iex(2)> Chronos.weeks_from(3)
+    {2013, 9, 11}
+  """
   def days_ago(days, date // :erlang.date) when days > 0 do
     calculate_date_for_days(date, -days)
   end
@@ -69,6 +149,16 @@ defmodule Chronos do
 
   defp _opts_date({ :ok, date }), do: date
 
+  @doc """
+    There is an option to supply a date. This is handy for testing.
+
+    defmodule YourModule do
+      use Chronos, date: {2012, 12, 21}
+    end
+
+    iex(1)> YourModule.today
+    {2012, 12, 21}
+  """
   defmacro __using__(opts // []) do
     date = fn() -> cond do
         opts[:date] ->
