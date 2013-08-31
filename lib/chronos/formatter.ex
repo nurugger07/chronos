7f3e6b70e0543866c1af15ce1bceeab47c382247
Dglgmut <florez.renan@gmail.com>
2013-08-31 18:45:45 -0300

    creating to_short_time_string format

--- lib/chronos/formatter.ex
+++ lib/chronos/formatter.ex
@@ -1,12 +1,17 @@
 defmodule Chronos.Formatter do
 
-  @default_format "~4..0B-~1..0B-~2..0B"
+  @default_date_format "~4..0B-~1..0B-~2..0B"
+  @default_time_format "~2..0B:~2..0B:~2..0B"
 
   def to_short_date_string(date) do
-    date_to_list(date) |> format_for(@default_format)
+    to_list(date) |> format_for(@default_date_format)
   end
 
-  defp date_to_list(date) when is_tuple(date), do: tuple_to_list(date)
+  def to_short_time_string(time) do
+    to_list(time) |> format_for(@default_time_format)
+  end
+
+  defp to_list(date) when is_tuple(date), do: tuple_to_list(date)
 
   defp format_for(date, format) when is_list(date) do
     :io_lib.format(format, date) |> iolist_to_binary
--- test/chronos/formatter_test.exs
+++ test/chronos/formatter_test.exs
@@ -4,9 +4,15 @@ defmodule FormatterTest do
   import Chronos.Formatter
 
   @today :erlang.date
+  @now :erlang.time
 
   test :short_date_string do
     { year, month, day } = @today
-    assert @today |> to_short_date_string == "#{year}-#{month}-#{day}"
+    assert @today |> to_short_date_string == "#{year}-#{:io_lib.format("~2..0B",[month])}-#{:io_lib.format("~2..0B",[day])}"
+  end
+
+  test :short_time_string do
+    { hour, minute, second } = @now
+    assert @now |> to_short_time_string == "#{hour}:#{:io_lib.format("~2..0B",[minute])}:#{:io_lib.format("~2..0B",[second])}"
   end
 end
