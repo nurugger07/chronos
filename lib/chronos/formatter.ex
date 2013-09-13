1038fd1f3ee40543a997ad78a98dad02deb07372
Hashrocket Workstation <dev@hashrocket.com>
2013-09-13 16:59:33 -0400

    Simple date formatting

--- lib/chronos/formatter.ex
+++ lib/chronos/formatter.ex
@@ -1,3 +1,9 @@
 defmodule Chronos.Formatter do
 
+  def strftime({ y, _, _ }, f) when f =="%Y", do: to_binary(y)
+  def strftime({ y, _, _ }, f) when f =="%y", do: rem(y, 100) |> to_binary
+
+  def strftime({ _, m, _ }, f) when f =="%m", do: to_binary(m)
+  def strftime({ _, _, d }, f) when f =="%d", do: to_binary(d)
+
 end
--- test/chronos/formatter_test.exs
+++ test/chronos/formatter_test.exs
@@ -1,3 +1,20 @@
 defmodule FormatterTest do
   use ExUnit.Case
+
+  @today { 2012, 12, 21 }
+
+  import Chronos.Formatter
+
+  test :strftime_dates do
+    # Year
+    assert strftime(@today, "%Y") == "2012"
+    assert strftime(@today, "%y") == "12"
+
+    # Month
+    assert strftime(@today, "%m") == "12"
+
+    # Day
+    assert strftime(@today, "%d") == "21"
+  end
+
 end
