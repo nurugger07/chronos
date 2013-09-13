0e05243330cc4b6414bf463979b5edec7a6fa9ab
Hashrocket Workstation <dev@hashrocket.com>
2013-09-13 16:23:39 -0400

    Remove short_date_string function

--- lib/chronos/formatter.ex
+++ lib/chronos/formatter.ex
@@ -1,14 +1,3 @@
 defmodule Chronos.Formatter do
 
-  @default_format "~4..0B-~1..0B-~2..0B"
-
-  def to_short_date_string(date) do
-    date_to_list(date) |> format_for(@default_format)
-  end
-
-  defp date_to_list(date) when is_tuple(date), do: tuple_to_list(date)
-
-  defp format_for(date, format) when is_list(date) do
-    :io_lib.format(format, date) |> iolist_to_binary
-  end
 end
--- test/chronos/formatter_test.exs
+++ test/chronos/formatter_test.exs
@@ -1,12 +1,3 @@
 defmodule FormatterTest do
   use ExUnit.Case
-
-  import Chronos.Formatter
-
-  @today :erlang.date
-
-  test :short_date_string do
-    { year, month, day } = @today
-    assert @today |> to_short_date_string == "#{year}-#{month}-#{day}"
-  end
 end
