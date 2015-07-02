b573c7d6ce4a406f6c73a4f77241c85499ad04db
Michael Schaarschmidt <michael@schaaryworks.com>
2015-07-02 11:02:07 +0200

    exchange :erlang.now with :os.timestamp
    Since Erlang/OTP 18.0 the function :erlang.now is deprecated
    (http://www.erlang.org/news/88) and should be exchanged with :os.timestamp.

--- test/chronos_test.exs
+++ test/chronos_test.exs
@@ -15,7 +15,7 @@ defmodule ChronosTest do
   end
 
   test :now do
-    assert :calendar.now_to_datetime(:erlang.now) == now
+    assert :calendar.now_to_datetime(:os.timestamp) == now
   end
 
   test :epoch_time do
